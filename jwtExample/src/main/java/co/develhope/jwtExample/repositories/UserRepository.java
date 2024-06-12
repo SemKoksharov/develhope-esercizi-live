4ee3791f0be40910f18b6c03339e9a910003554e
giansimo <gian_simo94@hotmail.it>
2024-06-12 17:18:00 +0200

    JWT Example

---
+++ jwtExample/src/main/java/co/develhope/jwtExample/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package co.develhope.jwtExample.repositories;
+
+import co.develhope.jwtExample.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
+}
